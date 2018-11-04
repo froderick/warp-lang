42b7b8eb0f1cf2250eff8c2c436c32045117e5f2
Michael Henry <froderick@gmail.com>
2018-11-04 08:23:04 -0500

    TTT: thomas says that daddy finished refactoring run-vm

--- interp/src/interp/core.clj
+++ interp/src/interp/core.clj
@@ -25,103 +25,120 @@
     frame
     (throw (Exception. (format "no current stack frame")))))
 
+(defn inc-ip
+  [vm]
+  (update-in vm [:ip] inc))
+
+(defn vm-push
+  [{:keys [stack frames] :as vm} [ref-type ref-arg :as args]]
+  (let [val (case ref-type
+
+              :args  ;; relative function argument index
+              (let [{:keys [saved-stack]} (current-frame vm)]
+                (when-not (integer? ref-arg)
+                  (throw (Exception. (format "arg-index must be an integer" args))))
+                (nth saved-stack ref-arg))
+
+              :local ;; relative local index
+              (let [{:keys [locals] :as frame} (current-frame vm)]
+                (when-not (integer? ref-arg)
+                  (throw (Exception. (format "local-index must be an integer" args))))
+                (get locals ref-arg))
+
+              :const ;; constant value
+              ref-arg)]
+
+    (log vm (format "push %s %s" ref-type val))
+
+    (-> vm
+        inc-ip
+        (assoc :stack (conj stack val)))))
+
+(defn vm-plus
+  [{:keys [stack frames] :as vm} [ref-type ref-arg]]
+  (let [b (first stack)
+        a (second stack)
+        result (+ a b)
+        stack (-> stack
+                  pop
+                  pop
+                  (conj result))]
+
+    (log vm (format "plus %s %s = %s" a b result))
+
+    (-> vm
+        inc-ip
+        (assoc :stack stack))))
+
+(defn vm-pop
+  [{:keys [stack frames] :as vm} [ref-type ref-arg :as args]]
+  (let [head (peek stack)]
+
+    (when-not head
+      (throw (Exception. (format "cannot pop stack, it is empty" args))))
+
+    (case ref-type
+
+      ;; pop to a local
+      :local (let [{:keys [locals] :as frame} (current-frame vm)
+                   local-index ref-arg]
+
+               (when-not (integer? local-index)
+                 (throw (Exception. (format "local-index must be an integer" args))))
+
+               (log vm (format "pop %s to local[%s]" head local-index))
+
+               (-> vm
+                   inc-ip
+                   (update-in [:frames (.indexOf frames frame) :locals] #(assoc % local-index head))
+                   (update-in [:stack] pop)))
+
+      ;; just pop the value
+      nil (do
+            (log vm "pop")
+            (-> vm
+                inc-ip
+                (assoc :stack (pop stack)))))))
+
+(defn vm-call
+  [{:keys [stack frames ip] :as vm} [ref-type new-ip]]
+  (log vm (format "call %s %s" ref-type new-ip))
+  
+  (case ref-type
+    :address 
+    (assoc vm
+           :ip new-ip
+           :frames (conj frames {:saved-stack stack
+                                 :saved-ip (inc ip)
+                                 :locals {}})
+           :stack [])))
+
+(defn vm-ret
+  [{:keys [stack frames] :as vm}]
+  (do
+    (log vm (format "ret"))
+
+    (let [{:keys [saved-stack saved-ip] :as frame} (current-frame vm)]
+       (assoc vm
+              :ip saved-ip
+              :frames (pop frames) 
+              :stack (if (seq stack)                       ;; if the stack is not empty, push the top item
+                       (conj saved-stack (peek stack))     ;; into the parent stack as a 'return' value
+                       saved-stack)))))
+
 (defn run-vm
-  [{:keys [vars stack heap instructions ip frames] :as vm}]
+  [{:keys [instructions ip] :as vm}]
 
-  (let [[next-inst next-args] (nth instructions ip)
-        next-vm (update-in vm [:ip] inc)]
+  (let [[inst args] (nth instructions ip)]
 
-    (case next-inst
+    (case inst
       :halt (log vm "halting")
-
-      :push (let [[ref-type ref-arg] next-args
-                  val (case ref-type
-
-                        :args  ;; relative function argument index
-                        (let [{:keys [saved-stack]} (current-frame vm)]
-                          (when-not (integer? ref-arg)
-                            (throw (Exception. (format "arg-index must be an integer" next-args))))
-                          (nth saved-stack ref-arg))
-
-                        :local ;; relative local index
-                        (let [{:keys [locals] :as frame} (current-frame vm)]
-                          (when-not (integer? ref-arg)
-                            (throw (Exception. (format "local-index must be an integer" next-args))))
-                          (get locals ref-arg))
-
-                        :const ;; constant value
-                        ref-arg)]
-
-              (log vm (format "push %s %s" ref-type val))
-              (recur (assoc next-vm :stack (conj stack val))))
-
-      :plus (let [[ref-type ref-arg] next-args
-                  b (first stack)
-                  a (second stack)
-                  result (+ a b)
-                  stack (-> stack
-                            pop
-                            pop
-                            (conj result))]
-
-              (log vm (format "plus %s %s = %s" a b result))
-
-              (recur (assoc next-vm :stack stack)))
-
-      :pop (let [head (peek stack)
-                 [ref-type ref-arg] next-args]
-
-             (when-not head
-               (throw (Exception. (format "cannot pop stack, it is empty" next-args))))
-
-             (case ref-type
-
-               ;; pop to a local
-               :local (let [{:keys [locals] :as frame} (current-frame vm)
-                            local-index ref-arg]
-
-                        (when-not (integer? local-index)
-                          (throw (Exception. (format "local-index must be an integer" next-args))))
-
-                        (log vm (format "pop %s to local[%s]" head local-index))
-
-                        (recur
-                         (-> next-vm
-                             (update-in [:frames (.indexOf frames frame) :locals] #(assoc % local-index head))
-                             (update-in [:stack] pop))))
-
-               ;; just pop the value
-               nil (do
-                     (log vm "pop")
-                     (recur (assoc next-vm :stack (pop stack))))))
-      
-
-      :call (let [[ref-type new-ip] next-args]
-              (log vm (format "call %s %s" ref-type new-ip))
-              
-              (case ref-type
-                :address 
-                (recur
-                 (assoc next-vm
-                        :frames (conj frames {:saved-stack stack
-                                              :saved-ip (inc ip)
-                                              :locals {}})
-                        :stack []
-                        :ip new-ip))))
-
-      :ret (do
-             (log vm (format "ret"))
-
-             (let [{:keys [saved-stack saved-ip] :as frame} (current-frame vm)]
-               (recur
-                (assoc vm
-                       :frames (pop frames) 
-                       :stack (if (seq stack)                       ;; if the stack is not empty, push the top item
-                                (conj saved-stack (peek stack))     ;; into the parent stack as a 'return' value
-                                saved-stack)
-                       :ip saved-ip))))
-
-      (log vm (format "invalid instruction %s, halting" next-inst)))))
+      :push (recur (vm-push vm args))
+      :plus (recur (vm-plus vm args))
+      :pop  (recur (vm-pop  vm args))
+      :call (recur (vm-call vm args))
+      :ret  (recur (vm-ret  vm))
+      (log vm (format "invalid instruction %s %s, halting" inst args)))))
 
 
 ;; what is missing
