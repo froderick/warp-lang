738fd3a2e11f7526ee8d471004546a849588ce93
Michael Henry <froderick@gmail.com>
2019-01-31 20:18:51 -0500

    thinking about expander

---
+++ expander.h
@@ -0,0 +1,30 @@
+#ifndef WARP_LANG_EXPANDER_H
+#define WARP_LANG_EXPANDER_H
+
+#include "reader.h"
+#include "vm.h"
+
+/*
+ * Things that have to be done to implement macros:
+ * - create builtin support to 'mark' vars as containing valid macro functions
+ * - implement expander, which can use a VM instance to locate macros, invoke them, and return the results
+ * - pass the expander to the analyzer as a parameter, make the analyzer use it when it can't find a binding for a symbol
+ *   - if it returns a NULL result, the symbol must refer to a var
+ *   - if it returns a non-NULL result, the symbol refers to a macro
+ *     - the resulting expression should replace the original one in the expr tree
+ *     - the old expression should be freed
+ *     - analysis should continue to recurse into the new expression
+ *
+ * - how can macroexpand be implemented?
+ *   - I guess this is part of implementing (eval), which has a superset of the same requirements
+ *   - the VM has to support a builtin that invokes the reader, analyzer, compiler, vm evaluation, etc.
+ */
+
+typedef struct Expander Expander_t;
+
+RetVal tryMakeExpander(Expander_t *expander, VM_t vm);
+void freeExpander(Expander_t expander);
+
+RetVal tryExpand(Expander_t expander, Text sym, Expr *input, Expr *output, Error *error);
+
+#endif //WARP_LANG_EXPANDER_H
