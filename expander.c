9ebfe0c6333619085d5a78d867fd376e2aac816a
Michael Henry <froderick@gmail.com>
2019-04-09 08:09:18 -0400

    clean up expander notes

--- expander.c
+++ expander.c
@@ -138,37 +138,6 @@ RetVal tryExpand(Expander *expander, Text sym, Expr *input, VMEvalResult *output
   throws(tryCompileTopLevel(root, &codeUnit, error));
   throws(tryVMEval(expander->vm, &codeUnit, output, error));
 
-  /*
-   * TODO: when we invoke eval here, we get back an expanded expression that has no line numbers associated with it at all
-   *
-   * Find a way for eval to save the known line numbers.
-   * - maybe include them somehow as metadata on the exprs such that when the macro references them, the lines are
-   *   preserved
-   * - or find a way to match up the expanded form of the exprs to the unexpanded forms and add back the missing
-   *   line numbers by passing through some kind of expr object reference id.
-   *
-   * *Either way, this is going to involve a re-work of the expander.*
-   *
-   * There might be another way. All the line numbers that apply to a given macro-expansion area already included in
-   * the code unit for doing the macro-expansion. That means if the VM wanted, it could look at them and reuse them.
-   * The trouble is that these line numbers become invalid because they are based on code-offsets, which will likely
-   * be changed by the new macro output.
-   *
-   * Let's think about this some more:
-   * - we would only expect to get line numbers in macro output where a macro emits values that directly correspond 1:1
-   *   with code the user wrote. This would only happen when the macro defines an fn?
-   *
-   * What if thi mostly doesn't matter? line numbers are for the code that was written, not the code that was generated.
-   * Shouldn't it be fine to pretend that the line numbers are correct?
-   *
-   * If an expr A is rewritten into expr B, it should still get the source lines for expr A. We dont need new source
-   * lines that are B-specific. But, we do need to know what expresssions in B refer to source lines from A. Which are
-   * those? How do we get back partial source lines?
-   *
-   *
-   * X expr -> X constant -> value -> macro-fn -> value -> expr
-   */
-
   return R_SUCCESS;
 
   failure:
